da1e0d4a216eed31906d69b3277136d9fcc6cff5
reireynoso <reireynoso@gmail.com>
2020-01-15 16:42:33 -0500

    change HTML tags and add CSS styling with JS file

---
+++ index.js
@@ -0,0 +1,12 @@
+const userInfo = {
+    id: 101,
+    email: 'rei@dev.com',
+    personalInfo: {
+        name: 'Rei',
+        address: {
+            line1: '81 Prospect St.',
+            city: 'Brooklyn',
+            state: 'NY'
+        }
+    }
+}
